83f3cd988503e6c4f1bb0648085100f179110fc5
Ricardo Ruiz <ricardo@kenetic-labs.com>
2015-06-26 21:29:12 -0700

    Initial Commit

---
+++ VOIP-Beta/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VOIP-Beta
+//
+//  Created by Ricardo Ruiz on 6/26/15.
+//  Copyright (c) 2015 Kenetic Labs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ VOIP-Beta/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VOIP-Beta
+//
+//  Created by Ricardo Ruiz on 6/26/15.
+//  Copyright (c) 2015 Kenetic Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
